f0ded6dac2f4d95b2f4b221e434ae9c4704092fa
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-05-22 10:46:22 -0500

    Feat(quizz): Implementar lógica de perguntas e seleção de respostas
    - Mapear opções dinamicamente no template usando *ngFor
    - Adicionar evento (click) para capturar a opção escolhida e armazenar no array de respostas
    - Importar dados do quiz a partir de um arquivo JSON externo
    - Implementar navegação entre perguntas com controle de índice
    - Marcar o quiz como finalizado quando todas as perguntas forem respondidas
    - Realizar ajustes no ngOnInit para carregar dados, definir título e perguntas do quiz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../../public/assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -14,12 +15,38 @@ export class QuizzComponent implements OnInit {
 
   answers: string[] = [];
   answerSelected: string = '';
-  
+
   questionIndex: number = 0;
   questionMaxIndex: number = 0;
   finished: boolean = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished == false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionsSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+      console.log(this.questionIndex);
+      console.log(this.questionMaxIndex);
+    }
+  }
+
+  playerChoose(value: string) {
+    this.answers.push(value);
+    console.log(this.answers);
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionsSelected = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+    }
+  }
 }
